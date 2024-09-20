beabcd721176892bec50513d35c79ccc4ceb619a
neilmarmahinay <neilmarmahinay@gmail.com>
2024-09-20 08:38:12 +0800

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,78 @@
+# Pokedex App
+[![Visit My Website](https://img.shields.io/badge/Visit%20My-Website-blue?style=for-the-badge&logo=arrow-right&logoColor=white)](https://lazyjinchuriki.github.io/pokedex/)
+
+Pokedex is a web application that serves as a comprehensive guide to Pokemon. It allows users to search for information on different Pokemon species, including their characteristics, abilities, types, and evolutions. The website is built using basic HTML, CSS, and vanilla JavaScript, and it utilizes the PokeAPI to fetch data on Pokemon.
+
+## Tech Stacks
+
+ ![HTML5](https://img.shields.io/badge/-HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white) ![CSS3](https://img.shields.io/badge/-CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white) ![JavaScript](https://img.shields.io/badge/-JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black) 
+
+## Features
+
+Pokedex offers the following features:
+
+- Search for Pokemon by name or Pokedex number
+- Get detailed information about a specific Pokemon, including its types, abilities, stats, and evolutions
+- View a list of all Pokemon and browse through them
+- Filter Pokemon based on their types
+- View popular Pokemon and their rankings
+- View top-rated Pokemon based on their stats
+- Explore Pokemon evolutions and their evolutionary paths
+
+## Installation
+
+To install Pokedex, follow these steps:
+
+1. Download the source code from the repository: https://github.com/lazyjinchuriki/pokedex
+2. Extract the files from the downloaded ZIP archive.
+3. Open the `index.html` file in your web browser.
+
+## Usage
+
+Once you have installed Pokedex, you can use it by opening it in your web browser. You can search for Pokemon using the search bar or browse through the list of all Pokemon. Clicking on a Pokemon will display its detailed information, including its types, abilities, stats, and evolutions.
+
+## Screenshots
+
+Here are some screenshots of Pokedex in action:
+
+| Homepage                        |
+|---------------------------------|
+| ![Homepage screenshot](/screenshots/homepage.png) |
+
+| Pokemon Overview               | Mobile Version                 |
+|--------------------------------|--------------------------------|
+| ![Pokemon search screenshot](/screenshots/overview.jpg) | ![Mobile screenshot](/screenshots/mobile.jpg) |
+
+| Pokemon Stats                   | Pokemon Evolution              |
+|---------------------------------|--------------------------------|
+| ![Pokemon details screenshot](/screenshots/stats.jpg) | ![Pokemon details screenshot](/screenshots/evolution.jpg) |
+
+
+## Credits
+
+Pokedex uses the following open source libraries and APIs:
+
+- PokeAPI: https://pokeapi.co/
+
+## Contributing
+
+Contributions to Pokedex are always welcome. If you find any bugs or issues, please open an issue on GitHub or submit a pull request.
+
+## Contact
+
+If you have any questions or concerns, you can contact the author of this project at [rahulkhushalani@proton.me](mailto:rahulkhushalani@proton.me).
+
+### Donations
+
+If you found Gallery useful, consider buying me a coffee!
+
+<a href="https://www.buymeacoffee.com/lazyjinchuriki">
+  <img src="https://www.buymeacoffee.com/assets/img/guidelines/download-assets-sm-1.svg" width="150" height="50">
+</a>
+
+## Social
+
+If you're not coding right now, check out my socials:
+
+[![Instagram](https://img.shields.io/badge/Instagram-%40amundaneguy-E4405F?style=for-the-badge&logo=instagram&logoColor=white)](https://www.instagram.com/amundaneguy/)
+[![LinkedIn](https://img.shields.io/badge/LinkedIn-Rahul%20Khushalani-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/rahul-khushalani-77ab21201/)
---
+++ script.js
@@ -0,0 +1,293 @@
+const poke_container = document.getElementById("poke-container");
+const pokemon_count = 1010;
+const colors = {
+  fire: "#e03a3a",
+  grass: "#50C878",
+  electric: "#fad343",
+  water: "#1E90FF",
+  ground: "#735139",
+  rock: "#63594f",
+  fairy: "#EE99AC",
+  poison: "#b34fb3",
+  bug: "#A8B820",
+  dragon: "#fc883a",
+  psychic: "#882eff",
+  flying: "#87CEEB",
+  fighting: "#bf5858",
+  normal: "#D2B48C",
+  ghost: "#7B62A3",
+  dark: "#414063",
+  steel: "#808080",
+  ice: "#98D8D8",
+};
+const regions = {
+  kanto: {
+    start: 1,
+    end: 151,
+  },
+  johto: {
+    start: 152,
+    end: 251,
+  },
+  hoenn: {
+    start: 252,
+    end: 386,
+  },
+  sinnoh: {
+    start: 387,
+    end: 493,
+  },
+  unova: {
+    start: 494,
+    end: 649,
+  },
+  kalos: {
+    start: 650,
+    end: 721,
+  },
+  alola: {
+    start: 722,
+    end: 809,
+  },
+  galar: {
+    start: 810,
+    end: 898,
+  },
+  hisui: {
+    start: 899,
+    end: 905,
+  },
+  paldea: {
+    start: 906,
+    end: 1010,
+  },
+};
+
+const loader = document.querySelector(".lds-ring");
+const fetchPokemons = async (region) => {
+  const { start, end } = regions[region];
+
+  loader.classList.add("ring-active");
+  
+  for (let i = start; i <= end; i++) {
+    const pokemonName = i.toString();
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
+
+    let res = await fetch(url);
+    let data = await res.json();
+    loader.classList.remove('ring-active')
+    createPokemonCard(data);
+    setTimeout(() => {
+    }, "150");
+  }
+};
+
+const main_types = Object.keys(colors);
+
+// const fetchPokemons = async () => {
+//   for (let i = 1; i <= pokemon_count; i++) {
+//     await getPokemon(i);
+//   }
+// };
+
+// const getPokemon = async (id) => {
+//   const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+//   const res = await fetch(url);
+//   const data = await res.json();
+//   console.log(data);
+//   createPokemonCard(data);
+// };
+
+const createPokemonCard = (pokemon) => {
+
+  const pokemonEl = document.createElement("div");
+  pokemonEl.classList.add("card");
+  pokemonEl.id = pokemon.id;
+
+  let name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+  if (name.length > 9) {
+    name = name.split("-")[0];
+  } else {
+    name = name;
+  }
+  const id = pokemon.id.toString().padStart(3, "0");
+  //    const moves = [];
+  //    try {
+  //     for (let i = 0; i <= 1 ; i++) {
+  //             moves.push(pokemon.moves[i].move.name);
+  //     }
+  //     console.log(moves);
+  //     } catch (error) {
+  //         console.log(error);
+  //     }
+
+  let weight = pokemon.weight / 10 + "kg";
+  let height = pokemon.height / 10 + "m";
+
+  const poke_types = pokemon.types.map((type) => type.type.name);
+  const type = main_types.find((type) => poke_types.indexOf(type) > -1);
+  const color = colors[type];
+  let frontImg;
+  let backImg;
+  try{
+    frontImg = pokemon.sprites.front_default;
+    backImg = pokemon.sprites.back_default;
+  }
+  catch(err){
+    frontImg = "#";
+    backImg = "#";
+  }
+
+  pokemonEl.style.backgroundColor = color;
+
+  const pokemonInnerHTML = `
+    <div class="front side">
+        <div class="img-container">
+        <img class="background" src="./Icons/default/pokeball.svg" alt="pokeball">
+        <img class="image" src="${frontImg}" alt="${name}">
+        </div>
+        <span class="number">#${id}</span>
+        <h3 class="name">${name}</h3>
+        <div class="types">
+          ${poke_types
+            .map(
+              (type) => `
+                <div class="poke__type__bg ${type}">
+                <img src="Icons/${type}.svg" alt="Type">
+                </div>
+          `
+            )
+            .join("")}
+        </div>
+    </div>
+    <div class="back side">
+        <div class="img-container">
+        <img class="image" src="${
+          backImg == null ? frontImg : backImg
+        }" alt="${name}" />
+        <img class="background" src="./Icons/default/pokeball.svg" alt="pokeball">
+        </div>
+        <span class="number">#${id}</span>
+    <div class="stats">
+    <div> Weight:<br> <b>${weight}</b></div>
+    <div> Height:<br> <b>${height}</b></div>
+    </div>
+    </div>
+  `;
+
+  // <div class="moves">
+  // <div>${moves[0]}</div>
+  // <div>${moves[1]}</div>
+  // </div>
+
+  pokemonEl.innerHTML = pokemonInnerHTML;
+  // Add event listener to open new page on card click
+  pokemonEl.addEventListener("click", () => {
+    // Open new page with specific card details
+    window.open(`details.html?id=${id}`, "_self");
+  });
+
+  const pokemonElHolder = document.createElement("div");
+  pokemonElHolder.classList.add("cardContainer");
+  pokemonElHolder.appendChild(pokemonEl);
+
+  poke_container.appendChild(pokemonElHolder);
+};
+
+const changeRegion = () => {
+  const regionSelect = document.getElementById("regionSelect");
+  regionSelect.addEventListener("click", (event) => {
+    const selectedRegion = event.target.getAttribute("data-value");
+    const activeRegion = document.querySelector(".active");
+    if (selectedRegion) {
+      poke_container.innerHTML = "";
+      fetchPokemons(selectedRegion);
+      activeRegion.classList.remove("active");
+      event.target.classList.add("active");
+    }
+  });
+};
+
+fetchPokemons("kanto");
+
+window.addEventListener("scroll", function () {
+  var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+  if (window.scrollY > 100) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+});
+
+document
+  .getElementById("scrollToTopBtn")
+  .addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+
+window.addEventListener("scroll", function () {
+  var scrollToDownBtn = document.getElementById("scrollToDownBtn");
+  if (window.scrollY > 100) {
+    scrollToDownBtn.style.display = "block";
+  } else {
+    scrollToDownBtn.style.display = "none";
+  }
+});
+
+document
+  .getElementById("scrollToDownBtn")
+  .addEventListener("click", function () {
+    window.scrollTo({
+      top: 999999,
+      behavior: "smooth",
+    });
+  });
+function search_pokemon() {
+  let input = document.getElementById("searchbar").value;
+  input = input.toLowerCase();
+  input = input.replace(/\s+/g, ""); // removing all spaces from search box
+  // storing all card along wiith details in variable
+  let x = document.getElementsByClassName("cardContainer");
+
+  for (i = 0; i < x.length; i++) {
+    // checking  the name or type entered by user from search box if doesn't match than dont display the message
+    if (!x[i].innerHTML.toLowerCase().includes(input)) {
+      x[i].style.display = "none";
+    }
+    // checking  the name or type entered by user from search box if doesn't match than dont display the pokemon card
+    else {
+      x[i].style.display = "block";
+    }
+  }
+}
+
+
+// dark mode enabled
+const darkModeButton = document.getElementById("dark");
+
+darkModeButton.addEventListener("click", () => {
+
+  let element = document.body;
+  element.classList.toggle("dark-mode");
+  document.body.classList.toggle("dark-mode");
+
+  const regions = document.querySelectorAll(".regionvalue");
+  console.log(regions);
+  regions.forEach(region => {
+    region.classList.toggle("dark-mode");
+  });
+
+});
+
+const darkModeIcon = document.getElementById("dark");
+darkModeButton.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+  darkModeIcon.classList.toggle("fa-toggle-on");
+  // You can add additional elements that need dark mode here
+});
+
+
+changeRegion();
